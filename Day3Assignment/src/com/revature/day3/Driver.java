0bc467f207b15e685feeab2c5938dca84958d8f6
Austin Coburn <austincoburn41@gmail.com>
2020-04-15 15:22:05 -0500

    Added a driver

---
+++ Day3Assignment/src/com/revature/day3/Driver.java
@@ -0,0 +1,16 @@
+package com.revature.day3;
+
+public class Driver {
+
+	public static void main(String[] args) {
+	
+		Dog d=new Dog();
+		d.setAge(12);
+		d.setBreed("Golden Retriever");
+		d.setSize("HUGE");
+		
+		System.out.println(d.getSize());
+
+	}
+
+}
