459a9727b3313a8e466749c9cbd56f3b38a51495
Zachary Patton <zacharypatton1991@gmail.com>
2020-04-15 16:12:09 -0400

    Modified Day3Assignment

---
+++ Day3Assignment/src/com/revature/zach/Driver.java
@@ -0,0 +1,20 @@
+package com.revature.zach;
+
+import com.revature.day3.Dog;
+import com.revature.day3.VideoGame;
+
+public class Driver {
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.setAge(2);
+		d.setBreed("German Shepherd");
+		d.setSize("80 lbs");
+		System.out.println("Alex the Dog:");
+		System.out.println(d);
+		
+		VideoGame vg = new VideoGame();
+		vg.setTitle("Halo");
+		vg.setGenre("Sci-fi FPS");
+		vg.setRating("T-M");
+	}
+}
